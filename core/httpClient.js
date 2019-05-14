4f266e942a78f4c07dca082c8b26a2189bec6ffd
Gazouly <m.algazouly96@gmail.com>
2019-05-14 17:26:00 +0200

    Fixing CORS problem

--- core/httpClient.js
+++ core/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const musixHttp = axios.create({
-  baseURL: 'https://api.musixmatch.com/ws/1.1/',
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/',
   params: {
     apikey: process.env.VUE_APP_API_KEY
   }
